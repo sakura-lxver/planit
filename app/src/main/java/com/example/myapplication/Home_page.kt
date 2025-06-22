dc56b453cfcec905c44d4e84950c6d271bb56e9c
Luqman <luqmannablan@gmail.com>
2025-06-21 19:27:53 -0500

    added Navbar

--- app/src/main/java/com/example/myapplication/Home_page.kt
+++ app/src/main/java/com/example/myapplication/Home_page.kt
@@ -1,32 +1,20 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class Home_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<Button>(R.id.button).setOnClickListener {
-
-           val intent = Intent(this, Home_page::class.java )
-            startActivity(intent)
-
-
-        }
     }
-
 }
